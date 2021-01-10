82e59a901fe9dddabc50da27b38539a808372680
Abhishek gupta <guptaabhishek46053@gmail.com>
2021-01-10 17:26:46 +0530

    changes in json

--- app.js
+++ app.js
@@ -1,50 +1,48 @@
-const express =require("express");
+const express = require("express");
 
-const path=require('path');
+const path = require("path");
 
-const mysql=require("mysql");
+const mysql = require("mysql");
 
-const app=express();
+const app = express();
 
-const dotenv=require('dotenv');
+const dotenv = require("dotenv");
 
-const cookieParser=require('cookie-parser');
+const cookieParser = require("cookie-parser");
 
-var port=process.env.PORT ;
+var port = process.env.PORT || 8000;
 
-dotenv.config({ path:'./.env'});
+dotenv.config({ path: "./.env" });
 
-
-const db =mysql.createConnection({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password:process.env.DATABASE_PASSWORD,
-    database:process.env.DATABASE
-
+const db = mysql.createConnection({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE,
 });
 
-const publicDirectory=path.join(__dirname,'./public');
+const publicDirectory = path.join(__dirname, "./public");
 
 app.use(express.static(publicDirectory));
 
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
 
-app.set('view engine','hbs');
+app.set("view engine", "hbs");
 
-db.connect( (error) => {
- if(error){
-     console.log(error)
- }else{
-     console.log("MYsql connected...")
- }
-})
+db.connect((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("MYsql connected...");
+  }
+});
 
 //define routes
-app.use('/',require('./routes/pages'));
-app.use('/auth',require('./routes/auth'));
+app.use("/", require("./routes/pages"));
+app.use("/auth", require("./routes/auth"));
 
-app.listen(port,()=>{
- console.log(`server started on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`server started on port ${port}`);
+});
